f8342245fb04d40bc944d29cfdf5343b0900742d
chen <184913704@qq.com>
2021-05-20 08:44:55 +0800

    可配置底部导航
    1

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,93 +1,111 @@
-// index.js
-// 获取应用实例
-const app = getApp()
-
-Page({
-  data: {
-    canIUseGetUserProfile: false,
-    searchValue:'',
-    vertical: false,
-    autoplay: false,
-    interval: 2000,
-    duration: 500,
-    images:['../../assess/images/1.jpeg','../../assess/images/2.jpeg','../../assess/images/3.jpeg'],
-    personImg:'../../assess/images/3.jpeg',
-    storeList:[
-      {},{},{}
-    ]
-  },
-  // 事件处理函数
-  // bindViewTap() {
-  //   wx.navigateTo({
-  //     url: '../logs/logs'
-  //   })
-  // },
-  onShow(){
-
-  },
-  onLoad() {
-    wx.getLocation({
-      type: 'wgs84',
-      success (res) {
-        const latitude = res.latitude
-        const longitude = res.longitude
-        const speed = res.speed
-        const accuracy = res.accuracy
-      }
-     })
-    wx.login({
-      success (res) {
-        if (res.code) {
-          //发起网络请求
-          // wx.request({
-          //   url: '',
-          //   data: {
-          //     code: res.code
-          //   }
-          // })
-        } else {
-          console.log('登录失败！' + res.errMsg)
-        }
-      }
-    })
-    
-    if (wx.getUserProfile) {
-      this.setData({
-        canIUseGetUserProfile: true
-      })
-    }
-  },
-  getUserInfo(e){
-    wx.getUserInfo({
-      lang: "zh_CN",
-      desc: '展示用户信息', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
-      success: (res) => {
-        console.log(res);
-      }
-    })
-  },
-  getUserProfile(e) {
-    // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认，开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
-    wx.getUserProfile({
-      desc: '展示用户信息', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
-      success: (res) => {
-        console.log(res)
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
+Component({
+  pageLifetimes: {
+    show() {
+      if (typeof this.getTabBar === 'function' &&
+        this.getTabBar()) {
+        this.getTabBar().setData({
+          selected: 1
         })
       }
-    })
-  },
-  goToStaffInfo(){
-    wx.navigateTo({
-      url: '../salesclerk/index',
-    })
-  },
-  goToStoreInfo(){
-    wx.navigateTo({
-      url: '../storeInfo/index',
-    })
+    }
   }
-  
 })
+// // index.js
+// // 获取应用实例
+// const app = getApp()
+
+// Page({
+//   data: {
+//     canIUseGetUserProfile: false,
+//     searchValue:'',
+//     vertical: false,
+//     autoplay: false,
+//     interval: 2000,
+//     duration: 500,
+//     images:['../../assess/images/1.jpeg','../../assess/images/2.jpeg','../../assess/images/3.jpeg'],
+//     personImg:'../../assess/images/3.jpeg',
+//     storeList:[
+//       {},{},{}
+//     ]
+//   },
+//   // 事件处理函数
+//   // bindViewTap() {
+//   //   wx.navigateTo({
+//   //     url: '../logs/logs'
+//   //   })
+//   // },
+//   onShow: function () {
+//     wx.setStorageSync('statu',0)
+//     if (typeof this.getTabBar === 'function' &&
+//       this.getTabBar()) {
+//       this.getTabBar().setData({
+//         selected: 0
+//       })
+//     }
+//   },
+//   onLoad() {
+//     wx.getLocation({
+//       type: 'wgs84',
+//       success (res) {
+//         const latitude = res.latitude
+//         const longitude = res.longitude
+//         const speed = res.speed
+//         const accuracy = res.accuracy
+//       }
+//      })
+//     wx.login({
+//       success (res) {
+//         if (res.code) {
+//           //发起网络请求
+//           // wx.request({
+//           //   url: '',
+//           //   data: {
+//           //     code: res.code
+//           //   }
+//           // })
+//         } else {
+//           console.log('登录失败！' + res.errMsg)
+//         }
+//       }
+//     })
+    
+//     if (wx.getUserProfile) {
+//       this.setData({
+//         canIUseGetUserProfile: true
+//       })
+//     }
+//   },
+//   getUserInfo(e){
+//     wx.getUserInfo({
+//       lang: "zh_CN",
+//       desc: '展示用户信息', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
+//       success: (res) => {
+//         console.log(res);
+//       }
+//     })
+//   },
+//   getUserProfile(e) {
+//     // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认，开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
+//     wx.getUserProfile({
+//       desc: '展示用户信息', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
+//       success: (res) => {
+//         console.log(res)
+//         this.setData({
+//           userInfo: res.userInfo,
+//           hasUserInfo: true
+//         })
+//       }
+//     })
+//   },
+//   goToStaffInfo(){
+//     wx.navigateTo({
+//       url: '../salesclerk/index',
+//     })
+//   },
+//   goToStoreInfo(){
+//     wx.navigateTo({
+//       url: '../storeInfo/index',
+//     })
+//   }
+  
+// })
--- pages/me/index.js
+++ pages/me/index.js
@@ -1,80 +1,97 @@
-// pages/me/index.js
-Page({
+// // pages/me/index.js
+// Page({
 
-	/**
-	 * 页面的初始数据
-	 */
-	data: {
-		avatarUrl:''
-	},
+// 	/**
+// 	 * 页面的初始数据
+// 	 */
+// 	data: {
+// 		avatarUrl:''
+// 	},
 
-	/**
-	 * 生命周期函数--监听页面加载
-	 */
-	onLoad: function (options) {
-		if(!wx.getStorageSync('userInfo')){
-			wx.navigateTo({
-			  url: '../loginPage/index',
-			})
-		}else{
-			let userInfo = wx.getStorageSync('userInfo')
-			console.log(userInfo)
-			this.setData({
-				avatarUrl :userInfo.avatarUrl
-			})
-		}
-	},
-	goToOrder(){
-		wx.switchTab({
-			url: '../order/index',
-		})
-	},
-	/**
-	 * 生命周期函数--监听页面初次渲染完成
-	 */
-	onReady: function () {
+// 	/**
+// 	 * 生命周期函数--监听页面加载
+// 	 */
+// 	onLoad: function (options) {
+// 		if(!wx.getStorageSync('userInfo')){
+// 			wx.navigateTo({
+// 			  url: '../loginPage/index',
+// 			})
+// 		}else{
+// 			let userInfo = wx.getStorageSync('userInfo')
+// 			console.log(userInfo)
+// 			this.setData({
+// 				avatarUrl :userInfo.avatarUrl
+// 			})
+// 		}
+// 	},
+// 	goToOrder(){
+// 		wx.switchTab({
+// 			url: '../order/index',
+// 		})
+// 	},
+// 	/**
+// 	 * 生命周期函数--监听页面初次渲染完成
+// 	 */
+// 	onReady: function () {
 
-	},
+// 	},
 
-	/**
-	 * 生命周期函数--监听页面显示
-	 */
-	onShow: function () {
+// 	/**
+// 	 * 生命周期函数--监听页面显示
+// 	 */
+// 	onShow: function () {
+//     if (typeof this.getTabBar === 'function' &&
+//       this.getTabBar()) {
+//       this.getTabBar().setData({
+//         selected: 2
+//       })
+//     }
+//   },
 
-	},
+// 	/**
+// 	 * 生命周期函数--监听页面隐藏
+// 	 */
+// 	onHide: function () {
 
-	/**
-	 * 生命周期函数--监听页面隐藏
-	 */
-	onHide: function () {
+// 	},
 
-	},
+// 	/**
+// 	 * 生命周期函数--监听页面卸载
+// 	 */
+// 	onUnload: function () {
 
-	/**
-	 * 生命周期函数--监听页面卸载
-	 */
-	onUnload: function () {
+// 	},
 
-	},
+// 	/**
+// 	 * 页面相关事件处理函数--监听用户下拉动作
+// 	 */
+// 	onPullDownRefresh: function () {
 
-	/**
-	 * 页面相关事件处理函数--监听用户下拉动作
-	 */
-	onPullDownRefresh: function () {
+// 	},
 
-	},
+// 	/**
+// 	 * 页面上拉触底事件的处理函数
+// 	 */
+// 	onReachBottom: function () {
 
-	/**
-	 * 页面上拉触底事件的处理函数
-	 */
-	onReachBottom: function () {
+// 	},
 
-	},
+// 	/**
+// 	 * 用户点击右上角分享
+// 	 */
+// 	onShareAppMessage: function () {
 
-	/**
-	 * 用户点击右上角分享
-	 */
-	onShareAppMessage: function () {
-
-	}
+// 	}
+// })
+Component({
+  pageLifetimes: {
+    show() {
+      if (typeof this.getTabBar === 'function' &&
+        this.getTabBar()) {
+        this.getTabBar().setData({
+          selected: 1
+        })
+      }
+    }
+  }
 })
